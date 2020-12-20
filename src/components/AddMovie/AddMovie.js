0d31168847c1f275df9662475963d3cf126ff29a
Jordan Newberry <jordan.newberry@gmail.com>
2020-12-19 19:52:52 -0600

    setting up material ui form in add movie component

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,13 +1,114 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// material-ui imports
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import MenuItem from '@material-ui/core/MenuItem';
+
+// Material-UI styles
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+});
 
 class AddMovie extends Component{
+
+    // local state
+    state = {
+        newMovie: {
+            title: '',
+            url: '',
+            description: '',
+            genres: []
+
+        }
+    }
     render(){
+        const classes = this.props.classes;
         return(
             <div>
-                <h2>hello from AddMovie component</h2>
+                <h2>Want to Add a New Movie?</h2>
+                <form className={classes.container}
+                        onSubmit={(event) => this.handleSubmit(event, this.state.newMovie)}>
+                    <TextField
+                        required
+                        id="standard-name"
+                        label="Title"
+                        className={classes.textField}
+                        value={this.state.newMovie.title}
+                        onChange={(event) => this.handleChange(event, 'title')}
+                        variant="outlined" />
+                    <TextField
+                        required
+                        id="standard-name"
+                        label="Image URL"
+                        className={classes.textField}
+                        value={this.state.newMovie.url}
+                        onChange={(event) => this.handleChange(event, 'url')}
+                        variant="outlined" />
+                    <TextField
+                        required
+                        id="outlined-multiline-static"
+                        label="Description"
+                        multiline
+                        rows="5"
+                        value={this.state.newMovie.description}
+                        onChange={(event) => this.handleChange(event, 'description')}
+                        className={classes.textField}
+                        margin="normal"
+                        variant="outlined"/>
+                    <Select
+                        value={this.state.newMovie.genres}
+                        onChange={(event) => this.handleChange(event, 'genres')}
+                        input={
+                        <OutlinedInput
+                            labelWidth={this.state.labelWidth}
+                            name="Genres"
+                            id="outlined-age-simple"
+                        />
+                        }
+                        >
+                        <MenuItem value="">
+                        <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={1}>Adventure</MenuItem>
+                        <MenuItem value={2}>Animated</MenuItem>
+                        <MenuItem value={3}>Biographical</MenuItem>
+                        <MenuItem value={4}>Comedy</MenuItem>
+                        <MenuItem value={5}>Disaster</MenuItem>
+                        <MenuItem value={6}>Drama</MenuItem>
+                        <MenuItem value={7}>Epic</MenuItem>
+                        <MenuItem value={8}>Fantasy</MenuItem>
+                        <MenuItem value={9}>Musical</MenuItem>
+                        <MenuItem value={10}>Romantic</MenuItem>
+                        <MenuItem value={11}>Sci-Fi</MenuItem>
+                        <MenuItem value={12}>Space-Opera</MenuItem>
+                        <MenuItem value={13}>Superhero</MenuItem>
+                    </Select>
+                </form>
             </div>
         ) // end return
     } // end render
 } // end class
 
-export default AddMovie;
+export default withStyles(styles)(AddMovie);
